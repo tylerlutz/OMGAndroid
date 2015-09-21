f5652f3a65e5196d8f471b822b90f907fd2da378
Tyler Lutz <tlutz.computers@gmail.com>
2015-09-20 20:25:29 -0400

    Initialized Git Repository
    Initial commit. Project has changed the layout and the EditText of the main activity.

---
+++ app/src/main/java/com/tylerlutz/omgandroid/MainActivity.java
@@ -0,0 +1,29 @@
+package com.tylerlutz.omgandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView mainTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
